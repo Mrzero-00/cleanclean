ca72d194aff6dcf0a7d3099917cb2a2bef8cc104
Mrzero-00 <mrzero885@gmail.com>
2022-11-24 01:10:54 +0900

    sms

--- src/hook/sms.js
+++ src/hook/sms.js
@@ -24,22 +24,17 @@ export const sendSms_cafe24 = (state)=>{
 
 export const sendSms_munja = (state)=>{
     // const formData = new FormData();
-    const buffer = encode('안녕하세요',"euc-kr").toString('utf-8');
     // formData.append('message',`고객명:${state.name}\n연락처:${state.tel}\n주소:${state.addres} ${state.detailAddres}\n일정:${state.date}\n건물 종류:${state.object}\n서비스 종류:${state.kind}\n평수:${state.area}\n방 갯수:${state.room}\n화장실 갯수:${state.bathRoom}\n베란다:${state.veranda}`);
-    // console.log("encode:",encode('안녕하세요',"EUC-KR").toString());
     axios({
       method: 'POST',
-      headers:{
-        "Content-Type": "text/html; charset=euc-kr"
-      },
-      url: 'https://www.munjanara.co.kr/MSG/send/web_admin_send.htm',
+      url: 'http://www.munjanara.co.kr/MSG/send/web_admin_send.htm',
       params:{
         userid:"youngh1991",
-        passwd:"cjdth10311",
+        passwd:"cjdth1031",
         sender:'01033356555',
         receiver:'01082371502',
-        encode:0,
-        message:buffer
+        // encode:0,
+        message:"안녕"
         // message:`고객명:${state.name}\n연락처:${state.tel}\n주소:${state.addres} ${state.detailAddres}\n일정:${state.date}\n건물 종류:${state.object}\n서비스 종류:${state.kind}\n평수:${state.area}\n방 갯수:${state.room}\n화장실 갯수:${state.bathRoom}\n베란다:${state.veranda}`
       }
     }).then((e)=>{console.log(e)})
